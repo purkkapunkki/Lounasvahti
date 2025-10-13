ccb189c5b2b45f37f01620019737ae66e812867e
purkkapunkki <daftm4il@hotmail.com>
2025-10-13 23:36:55 +0300

    Added dialog and new font

---
+++ js/dialog.js
@@ -0,0 +1,11 @@
+const dialog = document.querySelector('dialog');
+const showButton = document.querySelector('#show-dialog');
+const closeButton = document.querySelector('dialog button');
+
+showButton.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+  dialog.close();
+});
